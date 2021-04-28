4ec4b236c7f4202b8ccdd789b916f64ed425575e
Filip Jirsák <filip@jirsak.org>
2021-04-28 19:27:45 +0200

    Dynamická data – seznam osob

--- src/main/java/cz/czechitas/java2webapps/lekce5/controller/FamousPeopleController.java
+++ src/main/java/cz/czechitas/java2webapps/lekce5/controller/FamousPeopleController.java
@@ -1,16 +1,32 @@
 package cz.czechitas.java2webapps.lekce5.controller;
 
+import cz.czechitas.java2webapps.lekce5.entity.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Filip Jirsák
  */
 @Controller
 public class FamousPeopleController {
+  private final List<Person> people;
+
+  public FamousPeopleController() {
+    people = new ArrayList<>();
+    people.add(new Person("Angela", "Merkelová", LocalDate.of(1954, 7, 17)));
+    people.add(new Person("Bill", "Gates", LocalDate.of(1955, 10, 28)));
+    people.add(new Person("Greta", "Thunbergová", LocalDate.of(2003, 1, 3)));
+  }
 
   @GetMapping("/")
-  public String list() {
-    return "index";
+  public ModelAndView list() {
+    ModelAndView result = new ModelAndView("index");
+    result.addObject("people", people);
+    return result;
   }
 }
---
+++ src/main/java/cz/czechitas/java2webapps/lekce5/entity/Person.java
@@ -0,0 +1,45 @@
+package cz.czechitas.java2webapps.lekce5.entity;
+
+import java.time.LocalDate;
+
+/**
+ * Entita reprezentující osobu.
+ */
+public class Person {
+ private String givenName;
+ private String lastName;
+ private LocalDate birthDate;
+
+ public Person() {
+ }
+
+ public Person(String givenName, String lastName, LocalDate birthDate) {
+  this.givenName = givenName;
+  this.lastName = lastName;
+  this.birthDate = birthDate;
+ }
+
+ public String getGivenName() {
+  return givenName;
+ }
+
+ public void setGivenName(String givenName) {
+  this.givenName = givenName;
+ }
+
+ public String getLastName() {
+  return lastName;
+ }
+
+ public void setLastName(String lastName) {
+  this.lastName = lastName;
+ }
+
+ public LocalDate getBirthDate() {
+  return birthDate;
+ }
+
+ public void setBirthDate(LocalDate birthDate) {
+  this.birthDate = birthDate;
+ }
+}
